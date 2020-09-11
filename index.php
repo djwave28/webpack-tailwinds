dfaef4af0a9449de5c3bb6586d24ad4a2b188cb9
djwave28 <djwave28@gmail.com>
2020-09-11 07:27:33 -0700

    package maintenance

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div class="myUniqueClass my-48 px-12 bg-yellow-600">Example div class myUniqueClass</div>
     <p class="text-blue-300">And write a paragraph</p>
 
-    <div class="iDoNotExist"></div>
+    <div class="iDoNotExist">dome stuff</div>
 
     <script src="./dist/main.js"></script>
 </body>
