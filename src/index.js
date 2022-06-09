d063e4f6d0aa6fb51e1faf0395f0454e1a430383
Daniel Jongeling <djwave28@gmail.com>
2022-06-09 17:48:55 -0500

    new name

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import "./style.scss";
-import "./base.css";
-
-import { wizard } from "./wizard";
-console.log(wizard);
--- views/home/index.php
+++
@@ -1,6 +0,0 @@
-<div class="bg-green-200 p-4 space-y-4 max-w-xl mx-auto shadow-lg">
-    <p class="bg-green-200 text-green-600 p-4 mx-16 space-y-4 max-w-xl mx-auto shadow-l">Imported from Views</p>
-    <span class="block h-12 bg-green-500 rounded-lg px-4">1</span>
-    <span class="block h-12 bg-green-500 rounded-lg px-4">2</span>
-
-</div>
